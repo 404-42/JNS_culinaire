106456de49d9110910378cbb2facda744ed463d5
yoshibox <meddacisohan@gmail.com>
2021-03-13 13:49:24 +0100

    Added main class

--- src/Vue/Vue.java
+++ src/Vue/Vue.java
@@ -1,5 +1,12 @@
 package Vue;
 
-public class Vue {
+import Modèle.Modèle;
 
+public class Vue
+{
+	public static void main(String[] args)
+	{
+		// Changer d'emplacement quand le constructeur de Vue sera créer
+		Modèle modl = new Modèle();
+	}
 }
