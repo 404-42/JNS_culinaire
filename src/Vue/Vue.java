1498822e6e4da65629dd83e955ad057232d8e14a
404-42 <joakim.botti81@gmail.com>
2021-03-17 11:42:03 +0100

    Implémentation de javafx et création d'une première scène.

--- src/Vue/Vue.java
+++ src/Vue/Vue.java
@@ -1,12 +1,24 @@
 package Vue;
 
 import Modèle.Modèle;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
-public class Vue
-{
-	public static void main(String[] args)
-	{
+public class Vue extends Application {
+	public static void main(String[] args) {
 		// Changer d'emplacement quand le constructeur de Vue sera créer
 		Modèle modl = new Modèle();
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		BorderPane root = new BorderPane();
+		Scene scene = new Scene(root,400,400);
+		scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 }
