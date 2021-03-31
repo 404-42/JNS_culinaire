9535170d8a7ac770aa464715b7c4dadce7665c4a
yoshibox <meddacisohan@gmail.com>
2021-03-31 11:48:29 +0200

    Ajout d'un itérateur sur les recettes pour pouvoir les suivres

--- src/Modèle/Recette.java
+++ src/Modèle/Recette.java
@@ -4,8 +4,9 @@ import java.io.File;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.Iterator;
 
-public class Recette implements Comparable<Recette>, Serializable
+public class Recette implements Comparable<Recette>, Serializable, Iterable<Etape>
 {
 	private static final long serialVersionUID = 1L;
 	
@@ -38,6 +39,49 @@ public class Recette implements Comparable<Recette>, Serializable
 	public int compareTo(Recette arg0)
 	{
 		return this.nom.compareTo(arg0.nom);
+	}
+	
+	private class EtapesIterator implements Iterator<Etape>
+	{
+		private ArrayList<Etape> étapes;
+		int i;
+		
+		public EtapesIterator(Recette r)
+		{
+			this.étapes = r.etapes;
+			i = 0;
+		}
+
+		@Override
+		public boolean hasNext()
+		{
+			return i != étapes.size();
+		}
+
+		@Override
+		public Etape next()
+		{
+			// Retourne la valeur actuel et incrémente i après
+			return this.étapes.get(i++);
+		}
+		
+		public boolean hasPrevious()
+		{
+			return i != 0;
+		}
+		
+		public Etape previous()
+		{
+			// Décrémente i et retourne la valeur d'indice i-1
+			return this.étapes.get(--i);
+		}
+		
+	}
+
+	@Override
+	public Iterator<Etape> iterator()
+	{
+		return new EtapesIterator(this);
 	}
 
 }
